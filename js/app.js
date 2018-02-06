d87b2e0e56b07a00423a415ccfc8230617f23613
Elżbieta Neska <ela.neska@wp.pl>
2018-02-06 23:01:06 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,90 +1,55 @@
-// Select color input
-// Select size input
-
-
-
 $(function() {
 
+    $(document).on('click', '#buttonGrey', function () {
 
+        var rowVal = $('#row').val();
+        var colVal = $('#column').val();
 
-    function tablica(){
-        for(i=0; i<wiersze; i++){
-            $('#pixel_canvas').append("<tr></tr>");
+        $('tr').remove();
+        $('#addRule').remove();
+        $('.maximumGrid').remove();
 
-
-            $('tr').addClass("table");
-            for(j=0; j<kolumny; j++){
-
-                $('tr:last').append("<td></td>");
-            }
-        }
-    }
-
-    $(document).on('click', '#button', function () {
-
-            $('tr').remove();
-            $('#wstaw').remove();
-            $('.wytnij').remove();
-
-        var wiersze = $('#wiersze').val();
-
-
-
-        if(wiersze>10){
-            $('#tekst').append("<p class='wytnij'>Maksymalna wartość wierszy to 10!</p>");
-            wiersze =10;
-            $('#wiersze').val(10);
-
+        if(rowVal>10){
+            $('#text').append("<p class='maximumGrid'>Maximal row amount is set to 10</p>");
+            rowVal =10;
+            $('#row').val(10);
         };
 
-        var kolumny = $('#kolumny').val();
-        if(kolumny>10){
-            $('#tekst').append("<p class='wytnij'>Maksymalna wartość kolumn to 10!</p>");
-            kolumny =10;
-            $('#kolumny').val(10);
+        if(colVal>10){
+            $('#text').append("<p class='maximumGrid'>Maximal column amount is set to 10</p>");
+            colVal =10;
+            $('#column').val(10);
         };
 
-
-        for(i=0; i<wiersze; i++){
-            $('#pixel_canvas').append("<tr></tr>");
-
-
-            $('tr').addClass("table");
-            for(j=0; j<kolumny; j++){
-
-                $('tr:last').append("<td></td>");
+        function tablica(){
+            for(i=0; i<rowVal; i++){
+                $('#pixel_canvas').append('<tr></tr>');
+                $('tr').addClass('table');
+                for(j=0; j<colVal; j++){
+                    $('tr:last').append("<td></td>");
+                }
             }
         }
-            $('#size').append("<span id='wstaw'></span>");
+        tablica();
 
-            $('#wstaw').append(wiersze+ " x " + kolumny);
-
-
-
+        $('#size').append("<span id='addRule'></span>");
+        $('#addRule').append(rowVal+ " x " + colVal);
 
     });
 
 
     var color;
-
     $(".colorPick").click(function () {
         color = $(this).css("background-color");
     });
 
-
     $(document).on('click', '.colorPick', function () {
         $(".colorPick").css("border", "2px solid black");
         $(this).css("border", "4px solid black");
-
     });
-
 
     $(document).on("click", 'td', function(event) {
         $(this).css("background-color", color);
-
     });
-
-
-
 });
 
