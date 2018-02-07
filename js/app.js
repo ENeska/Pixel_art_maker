733f9f5cafc4b5ad5b7a5002fa6f34b5fc613f6f
Elżbieta Neska <ela.neska@wp.pl>
2018-02-07 07:29:13 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -48,7 +48,7 @@ $(function() {
         $(this).css("border", "4px solid black");
     });
 
-    $(document).on("click", 'td', function(event) {
+    $(document).on("mousedown", 'td', function(event) {
         $(this).css("background-color", color);
     });
 });
